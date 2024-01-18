29a0f1d9e93fdacd879e5a533746d6dadb56d0cc
apsarabiswokarma <apsarabk94@gmail.com>
2024-01-18 14:31:44 +0545

    feat: authentication ui

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,83 @@
-import { redirect } from "next/navigation"
-import { getServerAuthSession } from "~/server/auth"
+import { type Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
 
-import LoginForm from "./login-form"
+import { cn } from "~/lib/utils"
+import { buttonVariants } from "~/components/ui/button"
+import { Icons } from "~/components/ui/icons"
 
-export default async function LoginPage() {
-  const session = await getServerAuthSession()
+import { UserAuthForm } from "./user-auth-form"
 
-  if (!!session?.user) {
-    redirect("/")
-  }
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+}
 
+export default function AuthenticationPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <LoginForm />
-    </div>
+    <>
+      <div className="container relative flex min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          href="/examples/authentication"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          Login
+        </Link>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+          <div
+            className="absolute inset-0"
+            style={{
+              backgroundImage: "url(background.jpg)",
+              backgroundSize: "cover",
+            }}
+          />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <Icons.logo mode="dark"></Icons.logo>
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;Evento makes it so easy to manage and organise
+                events&rdquo;
+              </p>
+              <footer className="text-sm">Rupesh Budhathoki</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="h-full p-8">
+          <div className="mx-auto flex h-full w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Create an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to create your account
+              </p>
+            </div>
+            <UserAuthForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
