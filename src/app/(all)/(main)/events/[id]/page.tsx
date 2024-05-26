b7eaab2197b4e9e1f817bb4f50a84da8d4ac9461
apsarabishwokarma <apsarabk94@gmail.com>
2024-05-26 15:47:44 +0545

    feat: share event ui

--- src/app/(all)/(main)/events/[id]/page.tsx
+++ src/app/(all)/(main)/events/[id]/page.tsx
@@ -5,6 +5,7 @@ import { Text } from "@/components/ui/text"
 import EventCarousel from "@/components/event/event-carousel"
 import EventGrid from "@/components/event/event-grid"
 import InviteMembersButton from "@/components/event/invite-members"
+import ShareEvent from "@/components/event/share-event"
 
 export default async function EventDetails({
   params,
@@ -35,17 +36,41 @@ export default async function EventDetails({
           ]}
           title={data?.title}
         />
-
-        <div className="m-6 space-y-2 md:mt-10">
-          <Text as="h1" variant={"h4"} medium className="font-serif">
-            {data?.title}
-          </Text>
-          <Text variant={"medium"} medium className="mt-10 font-serif">
-            {data?.description}
-          </Text>
-          <p>{data?.capacity}</p>
-          <p>{data?.address}</p>
+        <div className="flex justify-between ">
+          <div className=" m-6 flex flex-col space-y-2 md:mt-10">
+            <Text as="h1" variant={"h4"} medium className="font-serif">
+              {data?.title}
+            </Text>
+            {/* <Text as="h1" variant={"h4"} medium className="font-serif">
+            📅 {data?.date}
+            </Text> */}
+            <Text variant={"medium"} medium className="mt-10 font-serif">
+              {data?.description}
+            </Text>
+            <p>{data?.capacity}</p>
+            <p>{data?.address}</p>
+          </div>
+          <div>
+            <div className="md:max-w-screen mt-5 w-full px-6 sm:px-4 ">
+              <div className="mb-5">
+                <h2 className="text-xl font-semibold">Event Location</h2>
+              </div>
+              <div className="rounded-lg bg-gray-100 p-4 ">
+                <div className="mb-4">
+                  <h3 className="text-lg font-medium">Lokanthali,Ram Mandir</h3>
+                  <p>Bhaktapur, Nepal</p>
+                </div>
+                <div className="flex h-64 items-center justify-center rounded-lg bg-gray-300">
+                  <span className="text-gray-700">Map Rakhne </span>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <ShareEvent />
+            </div>
+          </div>
         </div>
+
         <Separator className="my-10" />
 
         <div className="space-y-5">
