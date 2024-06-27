416d56931775863d6862ac5ac277438b59fddc23
roopaish <hi@roopaish.com>
2024-06-27 18:51:58 +0545

    fix: try catch seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,144 +14,152 @@ async function main() {
 }
 
 async function createUsers() {
-  const emailsToCreateAccountFor: string[] = process.env.EMAILS
-    ? (eval(process.env.EMAILS) as string[])
-    : []
+  try {
+    const emailsToCreateAccountFor: string[] = process.env.EMAILS
+      ? (eval(process.env.EMAILS) as string[])
+      : []
 
-  if (emailsToCreateAccountFor.length === 0) {
-    console.log(
-      "Please add EMAILS to .env file to create users with the provided emails."
-    )
-    return
-  }
+    if (emailsToCreateAccountFor.length === 0) {
+      console.log(
+        "Please add EMAILS to .env file to create users with the provided emails."
+      )
+      return
+    }
 
-  const existingUsers = await prisma.user.findMany({
-    where: {
-      email: {
-        in: emailsToCreateAccountFor,
+    const existingUsers = await prisma.user.findMany({
+      where: {
+        email: {
+          in: emailsToCreateAccountFor,
+        },
       },
-    },
-  })
+    })
 
-  if (existingUsers.length > 0) {
-    console.log("Users already exists with the provided emails.")
-    return existingUsers
+    if (existingUsers.length > 0) {
+      console.log("Users already exists with the provided emails.")
+      return existingUsers
+    }
+
+    const data: Prisma.UserCreateManyAndReturnArgs["data"] =
+      emailsToCreateAccountFor.map((email) => ({
+        email,
+        name: email.split("@")[0],
+        address: faker.location.streetAddress(),
+        bio: faker.lorem.paragraph(),
+        emailVerified: new Date(),
+        image: faker.image.avatar(),
+        role: "USER",
+      }))
+
+    const users = await prisma.user.createManyAndReturn({
+      data,
+    })
+
+    return users
+  } catch (e) {
+    console.log(e)
   }
-
-  const data: Prisma.UserCreateManyAndReturnArgs["data"] =
-    emailsToCreateAccountFor.map((email) => ({
-      email,
-      name: email.split("@")[0],
-      address: faker.location.streetAddress(),
-      bio: faker.lorem.paragraph(),
-      emailVerified: new Date(),
-      image: faker.image.avatar(),
-      role: "USER",
-    }))
-
-  const users = await prisma.user.createManyAndReturn({
-    data,
-  })
-
-  return users
 }
 
 async function createProperties(
   users: NonNullable<Awaited<ReturnType<typeof createUsers>>>
 ) {
-  const data: Prisma.EventCreateArgs["data"][] = []
+  try {
+    const data: Prisma.EventCreateArgs["data"][] = []
 
-  for (let i = 0; i < 100; i++) {
-    const categoryKeys = Object.keys(eventTags)
-    const randomCategory = categoryKeys[
-      Math.floor(Math.random() * categoryKeys.length)
-    ] as keyof typeof eventTags
-    const tags = eventTags[randomCategory]
+    for (let i = 0; i < 100; i++) {
+      const categoryKeys = Object.keys(eventTags)
+      const randomCategory = categoryKeys[
+        Math.floor(Math.random() * categoryKeys.length)
+      ] as keyof typeof eventTags
+      const tags = eventTags[randomCategory]
 
-    const user = users[Math.floor(Math.random() * users.length)]
+      const user = users[Math.floor(Math.random() * users.length)]
 
-    const selectedTags = tags
-      .sort(() => 0.5 - Math.random())
-      .slice(0, Math.floor(Math.random() * 3) + 3)
+      const selectedTags = tags
+        .sort(() => 0.5 - Math.random())
+        .slice(0, Math.floor(Math.random() * 3) + 3)
 
-    data.push({
-      title: faker.company.catchPhrase(),
-      type: "OTHER",
-      category: randomCategory,
-      date: faker.date.future(),
-      address: faker.location.streetAddress(),
-      lat: faker.location.latitude().toString(),
-      lng: faker.location.longitude().toString(),
-      capacity: faker.number.int({ min: 50, max: 500 }),
-      description: faker.lorem.paragraphs(2),
-      instruction: faker.lorem.sentence(),
-      price: Number(faker.commerce.price({ min: 10, max: 200, dec: 2 })),
-      managerName: faker.person.fullName(),
-      managerPhone: faker.phone.number(),
-      managerEmail: faker.internet.email(),
-      managerImage: {
-        create: {
-          id: faker.number.int().toString(),
-          name: faker.lorem.word(),
-          url: faker.image.avatar(),
-          thumbnailUrl: faker.image.url(),
-          size: 22,
-          createdAt: faker.date.past(),
-          updatedAt: faker.date.recent(),
-          user: {
-            connect: {
-              id: user!.id,
+      data.push({
+        title: faker.company.catchPhrase(),
+        type: "OTHER",
+        category: randomCategory,
+        date: faker.date.future(),
+        address: faker.location.streetAddress(),
+        lat: faker.location.latitude().toString(),
+        lng: faker.location.longitude().toString(),
+        capacity: faker.number.int({ min: 50, max: 500 }),
+        description: faker.lorem.paragraphs(2),
+        instruction: faker.lorem.sentence(),
+        price: Number(faker.commerce.price({ min: 10, max: 200, dec: 2 })),
+        managerName: faker.person.fullName(),
+        managerPhone: faker.phone.number(),
+        managerEmail: faker.internet.email(),
+        managerImage: {
+          create: {
+            id: faker.number.int().toString(),
+            name: faker.lorem.word(),
+            url: faker.image.avatar(),
+            thumbnailUrl: faker.image.url(),
+            size: 22,
+            createdAt: faker.date.past(),
+            updatedAt: faker.date.recent(),
+            user: {
+              connect: {
+                id: user!.id,
+              },
             },
           },
         },
-      },
-      createdById: user!.id,
-      tags: {
-        connectOrCreate: selectedTags.map((tag) => ({
-          where: {
-            name: tag,
-          },
-          create: { name: tag },
-        })),
-      },
-      assets: {
-        create: {
-          id: faker.number.int().toString(),
-          name: faker.lorem.slug(),
-          url: faker.image.urlLoremFlickr({ category: randomCategory }),
-          thumbnailUrl: faker.image.url(),
-          size: 22,
-          createdAt: faker.date.past(),
-          updatedAt: faker.date.recent(),
-          user: {
-            connect: {
-              id: user!.id,
+        createdById: user!.id,
+        tags: {
+          connectOrCreate: selectedTags.map((tag) => ({
+            where: {
+              name: tag,
+            },
+            create: { name: tag },
+          })),
+        },
+        assets: {
+          create: {
+            id: faker.number.int().toString(),
+            name: faker.lorem.slug(),
+            url: faker.image.urlLoremFlickr({ category: randomCategory }),
+            thumbnailUrl: faker.image.url(),
+            size: 22,
+            createdAt: faker.date.past(),
+            updatedAt: faker.date.recent(),
+            user: {
+              connect: {
+                id: user!.id,
+              },
             },
           },
         },
-      },
 
-      jobPositions: {
-        create: {
-          title: faker.person.jobTitle(),
-          description: faker.lorem.paragraph(),
-          salary: Number(faker.commerce.price()),
-          noOfEmployees: faker.number.int(),
+        jobPositions: {
+          create: {
+            title: faker.person.jobTitle(),
+            description: faker.lorem.paragraph(),
+            salary: Number(faker.commerce.price()),
+            noOfEmployees: faker.number.int(),
+          },
         },
-      },
-    }),
-      console.log(data)
-  }
+      }),
+        console.log(data)
+    }
 
-  await Promise.all(
-    data.map(async (d) => {
-      await prisma.event.create({
-        data: d,
+    await Promise.all(
+      data.map(async (d) => {
+        await prisma.event.create({
+          data: d,
+        })
       })
-    })
-  )
+    )
 
-  console.log("100 events created.")
+    console.log("100 events created.")
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 main()
