4369600910b93bdfbf479d42e30f901b5b46ce34
Bikram Pun <bikrampun47@gmail.com>
2024-06-29 17:24:48 +0545

    feat: get user api in server side

--- src/lib/validations/user-schema.ts
+++ src/lib/validations/user-schema.ts
@@ -1,5 +1,11 @@
 import * as z from "zod"
 
+function isValidCUID(cuid: string): boolean {
+  // Basic validation to check if the cuid is a string of 25 characters starting with 'c'
+  const cuidPattern = /^c[a-zA-Z0-9]{24}$/
+  return cuidPattern.test(cuid)
+}
+
 export const editProfileFormSchema = z.object({
   name: z.string().min(2, {
     message: "Name must be at least 3 characters.",
@@ -10,4 +16,9 @@ export const editProfileFormSchema = z.object({
   bio: z.string().optional(),
 })
 
+export const userIdSchema = z.object({
+  id: z.string().refine((id) => isValidCUID(id), "Invalid User ID"),
+})
+
 export type EditProfileForm = z.infer<typeof editProfileFormSchema>
+export type UserId = z.infer<typeof userIdSchema>
